b03d60b0d9f954b416c4888bcb764805251a052b
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-09-25 16:10:26 -0500

    JS code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello!");
+let count = 0;
+
+const updateCount = () => {
+  const countElem = document.querySelector('#count');
+  countElem.innerHTML = count;
+};
+
+const incButton = document.querySelector('#increment');
+const decButton = document.querySelector('#decrement');
+
+incButton.addEventListener("click", () => {
+  count += 1;
+  updateCount();
+});
+
+decButton.addEventListener("click", () => {
+  count -= 1;
+  updateCount();
+});
